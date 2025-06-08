26e5d9e3dd3944ebbe8f699caac9945ad755d683
alibeaj.flavio <alibeaj.flavio@gmail.com>
2025-06-08 23:36:54 +0200

    feat: enhance SelectSkip component with MatStepper and improved layout

--- src/pages/SelectSkip/SelectSkip.tsx
+++ src/pages/SelectSkip/SelectSkip.tsx
@@ -1,15 +1,30 @@
 import { useEffect, useState } from "react";
 import type { SkipType } from "../../model/SkipType";
 import { getSkips } from "../../http/skip.http";
+import { Box } from "@mui/material";
+import MatStepper from "../../components/MatStepper/MatStepper";
 
 const SelectSkip = () => {
   const [users, setUsers] = useState<SkipType[]>([]);
 
   useEffect(() => {
-    getSkips().then(({ data }) => setUsers(data));
+    getSkips()
+      .then(({ data }) => setUsers(data))
+      .catch(() => setUsers([]));
   }, []);
 
-  return users.map((u) => <p key={u.id}>{u.id} works!</p>);
+  return (
+    <Box
+      sx={{
+        paddingInline: "4rem",
+        paddingBlock: "2rem",
+      }}>
+      <MatStepper />
+      {users.map((u) => (
+        <p key={u.id}>{u.id} works!</p>
+      ))}
+    </Box>
+  );
 };
 
 export default SelectSkip;
